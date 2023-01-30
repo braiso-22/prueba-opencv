96e042a110680163ce7afb1145dab86038be12c7
Brais <braisfv22@gmail.com>
2023-01-30 17:17:30 +0100

    escala de grises

--- operaciones_aritmeticas.py
+++ operaciones_aritmeticas.py
@@ -6,9 +6,15 @@ from utils import Img
 
 def main():
     imagen = Img.cargar_imagen("img/banda_caja1.png")
+    imagen2 = Img.cargar_imagen("img/banda_caja1_5.png")
+    imagen3 = Img.cargar_imagen("img/banda_caja2.png")
     r, g, b = Img.separar_rgb(imagen)
     h, s, v = Img.separar_hsv(imagen)
     l, u, v = Img.separar_luv(imagen)
+
+    img = Img.escala_grises(imagen)
+    img2 = Img.escala_grises(imagen2)
+    img3 = Img.escala_grises(imagen3)
 
 
 if __name__ == '__main__':
