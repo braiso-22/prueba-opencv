437169142a83b3a12f4d1bc3aeba2f64070740b8
Brais <braisfv22@gmail.com>
2023-01-30 16:20:02 +0100

    refactor

--- prueba-opencv.py
+++ prueba-opencv.py
@@ -21,6 +21,18 @@ def print_image_info(image: np.ndarray):
     print(image.ndim)
 
 
+def separar_colores(image):
+    red, green, blue = cv.split(image)
+    # separar colores
+    figura, (eje1, eje2, eje3) = plt.subplots(1, 3)
+    eje1.imshow(red, cmap="gray", vmin=0, vmax=255)
+    eje2.imshow(green, cmap="gray")
+    eje3.imshow(blue, cmap="gray")
+
+    plt.show()
+    return red, green, blue
+
+
 def unir_colores(red, green, blue):
     return cv.merge([blue, green, red])
 
@@ -28,16 +40,9 @@ def unir_colores(red, green, blue):
 def main():
     image = cargar_imagen()
     print_image_info(image)
-    red, green, blue = cv.split(image)
-    # separar colores
-    figura, (eje1, eje2, eje3) = plt.subplots(1, 3)
-    eje1.imshow(red, cmap="gray")
-    eje2.imshow(green, cmap="gray")
-    eje3.imshow(blue, cmap="gray")
-    
+    red, green, blue = separar_colores(image)
     image = unir_colores(red, green, blue)
     plt.imshow(image)
-
     plt.show()
 
 
