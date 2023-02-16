7efdbb542d8102717069298d8d0a6d15f5172f86
Brais <braisfv22@gmail.com>
2023-02-16 17:51:19 +0100

    Programa que detecta el movimiento y tapa el fondo

---
+++ camara_movimiento.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+import numpy as np
+from utils import Camera
+
+
+def tapar_fondo(frame, img_fondo):
+    cv.accumulateWeighted(frame, img_fondo, 0.22)
+    img_ok = cv.convertScaleAbs(img_fondo)
+    dif = cv.absdiff(frame, img_ok)
+    dif = cv.cvtColor(dif, cv.COLOR_BGR2GRAY)
+    nada, mascara = cv.threshold(dif, 20, 255, cv.THRESH_BINARY)
+    mascara = cv.morphologyEx(
+        mascara,
+        cv.MORPH_CLOSE,
+        np.ones((5, 5), np.uint8),
+        iterations=7)
+    final = cv.bitwise_and(frame, frame, mask=mascara)
+    return final
+
+
+def main():
+    captura_video = cv.VideoCapture(1, cv.CAP_DSHOW)
+    cv.namedWindow("Video")
+    ret, img_fondo = captura_video.read()
+    img_fondo = np.float32(img_fondo)
+    accion = 1
+    while True:
+        frame = Camera.obtener_frame(captura_video)
+        final = tapar_fondo(frame, img_fondo)
+        accion = Camera.mostrar_frame(final, accion)
+        if accion == -1:
+            break
+        if accion == 0:
+            accion = 0
+        else:
+            accion = 1
+
+
+if __name__ == '__main__':
+    main()
