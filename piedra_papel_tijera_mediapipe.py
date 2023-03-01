1e721ae05f9620e146b35247a052b5d48698c5d3
Brais <braisfv22@gmail.com>
2023-03-01 18:40:48 +0100

    clase juego que guarda el estado y tiene funciones para cambiarlo

--- piedra_papel_tijera_mediapipe.py
+++ piedra_papel_tijera_mediapipe.py
@@ -4,41 +4,124 @@ import tensorflow as tf
 import numpy as np
 
 
-def menu_juego(image, entrada):
-    cv.putText(
-        image,
-        "Pulgar arriba para empezar, pulgar abajo para salir",
-        (50, 50),
-        cv.FONT_HERSHEY_SIMPLEX,
-        1,
-        (255, 255, 255),
-        2,
-        cv.LINE_AA
-    )
-    if entrada == "ok":
-        return 1
-    elif entrada == "no":
-        return -1
-    else:
-        return None
+class Juego:
+    def __init__(self):
+        self.opciones = ["piedra", "papel", "tijera"]
+        self.estado = 0
+        self.jugada_cpu = self.opciones[np.random.randint(0, 3)]
+        self.jugada_jugador = None
+        pass
 
+    def mostrar_menu_juego(self, image):
+        cv.putText(
+            image,
+            "Pulgar arriba para empezar, pulgar abajo para salir",
+            (50, 50),
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (255, 255, 255),
+            2,
+            cv.LINE_AA
+        )
 
-def jugada_cpu():
-    opciones = ["piedra", "papel", "tijera"]
-    return opciones[np.random.randint(0, 3)]
+    def opcion_menu_juego(self, entrada):
+        if entrada == "ok":
+            return 1
+        elif entrada == "no":
+            return -1
+        else:
+            return None
 
+    def resultado_jugada(self, player: str):
+        if player == self.jugada_cpu:
+            return "Empate"
+        elif player == "piedra" and self.jugada_cpu == "tijera":
+            return "Gana jugador"
+        elif player == "papel" and self.jugada_cpu == "piedra":
+            return "Gana jugador"
+        elif player == "tijera" and self.jugada_cpu == "papel":
+            return "Gana jugador"
+        else:
+            return "Gana CPU"
 
-def resultado_jugada(player, cpu):
-    if player == cpu:
-        return "Empate"
-    elif player == "piedra" and cpu == "tijera":
-        return "Gana jugador"
-    elif player == "papel" and cpu == "piedra":
-        return "Gana jugador"
-    elif player == "tijera" and cpu == "papel":
-        return "Gana jugador"
-    else:
-        return "Gana CPU"
+    def __rectangle__(self, image, text_len):
+        cv.rectangle(
+            image,
+            (50, 40),
+            (text_len * 10, 60),
+            (0, 0, 0),
+            cv.FILLED
+        )
+
+    def mostrar_menu_actual(self, image):
+        if self.estado == 0:
+            self.mostrar_menu_juego(image)
+        elif self.estado == 1:
+            string_a_mostrar = f"Jugada del jugador: {self.jugada_jugador}. Pulgar arriba para jugar"
+            self.__rectangle__(image, len(string_a_mostrar))
+            cv.putText(
+                image,
+                string_a_mostrar,
+                (50, 50),
+                cv.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (255, 255, 255),
+                2,
+                cv.LINE_AA
+            )
+        elif self.estado == 2:
+            string_a_mostrar = f"Jugada del jugador: {self.jugada_jugador}. Jugada de la CPU: {self.jugada_cpu}"
+            self.__rectangle__(image, len(string_a_mostrar))
+            cv.putText(
+                image,
+                string_a_mostrar,
+                (50, 50),
+                cv.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (255, 255, 255),
+                2,
+                cv.LINE_AA
+            )
+            cv.putText(
+                image,
+                f"{self.resultado_jugada(self.jugada_jugador)}",
+                (50, 150),
+                cv.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                (255, 255, 255),
+                2,
+                cv.LINE_AA
+            )
+        elif self.estado == 3:
+            string_a_mostrar = "Pulgar arriba para volver a jugar, pulgar abajo para salir"
+            self.__rectangle__(image, len(string_a_mostrar))
+            cv.putText(
+                image,
+                string_a_mostrar,
+                (50, 50),
+                cv.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (255, 255, 255),
+                2,
+                cv.LINE_AA
+            )
+        pass
+
+    def cambiar_estado_juego(self, posicion_actual):
+        if self.estado == 0:
+            self.estado = self.opcion_menu_juego(posicion_actual) if self.opcion_menu_juego(posicion_actual) else 0
+        elif self.estado == 1:
+            if posicion_actual == "ok" and is_valid_movement(self.jugada_jugador):
+                self.estado = 2
+            self.jugada_jugador = posicion_actual
+        elif self.estado == 2:
+            self.estado = 3
+        elif self.estado == 3:
+            if posicion_actual == "ok":
+                self.estado = 1
+                self.jugada_cpu = self.opciones[np.random.randint(0, 3)]
+            elif posicion_actual == "no":
+                self.estado = -1
 
 
 def cargar_modelo(route: str):
@@ -87,7 +170,7 @@ def is_valid_movement(movement):
     return movement in valid_movements
 
 
-def procesar_frame(image, hands, mp_drawing, mp_hands, modelo, nombres_posiciones, estado, movimiento_anterior):
+def procesar_frame(image, hands, mp_drawing, mp_hands, modelo, nombres_posiciones, juego, num_frames):
     image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     image.flags.writeable = False
     results = hands.process(image)
@@ -95,7 +178,6 @@ def procesar_frame(image, hands, mp_drawing, mp_hands, modelo, nombres_posicione
     image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
     posicion_actual = None
     if results.multi_hand_landmarks:
-
         for hand_landmarks in results.multi_hand_landmarks:
             landmarks_in_screen = landmarks_to_screen_points(hand_landmarks, image.shape[1], image.shape[0])
             mp_drawing.draw_landmarks(
@@ -107,30 +189,14 @@ def procesar_frame(image, hands, mp_drawing, mp_hands, modelo, nombres_posicione
 
         if translate_posicion(posicion_actual) != "":
             posicion_actual = translate_posicion(posicion_actual)
-        else:
-            posicion_actual = movimiento_anterior
         image = cv.flip(image, 1)
-        if estado == 0:
-            estado = menu_juego(image, posicion_actual) if menu_juego(image, posicion_actual) else 0
-        elif estado == 1:
-            cv.putText(
-                image,
-                f"Jugada del jugador: {movimiento_anterior}. Pulgar arriba para jugar",
-                (50, 50),
-                cv.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-                cv.LINE_AA
-            )
-            if posicion_actual == "ok" and is_valid_movement(movimiento_anterior):
-                estado = 2
-
-                pass
+        juego.mostrar_menu_actual(image)
+        if num_frames % 40 == 0:
+            juego.cambiar_estado_juego(posicion_actual)
     else:
         image = cv.flip(image, 1)
 
-    return image, estado, posicion_actual
+    return image, posicion_actual
 
 
 def capturar_video_mano():
@@ -150,8 +216,8 @@ def capturar_video_mano():
             min_detection_confidence=0.5,
             min_tracking_confidence=0.7
     ) as hands:
-        estado = 0
-        movimiento_anterior = None
+        juego = Juego()
+        frame_num = 0
         while cap.isOpened():
             # Leer frame
             success, image = cap.read()
@@ -159,12 +225,13 @@ def capturar_video_mano():
                 print("Ignoring empty camera frame.")
                 continue
             # procesar frame
-            image, estado, movimiento_anterior = procesar_frame(image, hands, mp_drawing, mp_hands, modelo,
-                                                                nombres_posiciones, estado, movimiento_anterior)
+            frame_num += 1
+            image, movimiento_anterior = procesar_frame(image, hands, mp_drawing, mp_hands, modelo,
+                                                        nombres_posiciones, juego, frame_num)
             # mostrar frame
             cv.imshow('MediaPipe Hands', image)
             # salir
-            if cv.waitKey(5) & 0xFF == 27 or estado == -1:
+            if cv.waitKey(5) & 0xFF == 27 or juego.estado == -1:
                 break
     cap.release()
 
